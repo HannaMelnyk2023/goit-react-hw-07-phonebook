4d9eeacba8d6c61cf668520adc0801ebefc15625
HannaMelnyk2023 <151830548+HannaMelnyk2023@users.noreply.github.com>
2026-02-18 23:23:45 +0200

    edit ContactForm

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,28 @@
-import { Toaster } from 'react-toastify';
-import { Switch, Route } from 'react-router-dom';
-import { ContactsPage } from 'pages/contacts';
-import { CreateContactPage } from 'pages/CreateContact';
-import { HomePage } from 'pages/HomePage';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchContacts } from "../redux/contacts/operations";
 
-export const App = () => {
+function App() {
+  const dispatch = useDispatch();
+  const contacts = useSelector((state) => state.contacts.items);
+  const isLoading = useSelector((state) => state.contacts.isLoading);
+  const error = useSelector((state) => state.contacts.error);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <>
-      <Switch>
-        <Route path="/" exact>
-          <HomePage />
-        </Route>
-        <Route path="/contacts" exact>
-          <ContactsPage />
-        </Route>
-        <Route path="/contacts/create" exact>
-          <CreateContactPage />
-        </Route>
-      </Switch>
-      <Toaster position="top-right" />
-    </>
+    <div>
+      <h1>Phonebook</h1>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+      <ul>
+        {contacts.map((contact) => (
+          <li key={contact.id}>{contact.name}: {contact.number}</li>
+        ))}
+      </ul>
+    </div>
   );
-};
+}
+export default App;
---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addContacts } from '../redux/contacts/operations';
+
+function ContactForm() {
+  const dispatch = useDispatch();
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleSubmit = e => {
+      e.preventDefault();
+      if (!name.trim() || !number.trim()) {
+          alert('Заповніть усе, будь ласка, а то шо попало!');
+          return;
+      }
+      
+    dispatch(addContacts({ name, number }));
+    setName('');
+    setNumber('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input
+          type="text"
+          value={name}
+          onChange={e => setName(e.target.value)}
+          required
+        />
+          </label>
+          <br/>
+      <label>
+        Number:
+        <input
+          type="text"
+          value={number}
+          onChange={e => setNumber(e.target.value)}
+          required
+        />
+          </label>
+          <br/>
+      <button type="submit">Add Contact</button>
+    </form>
+  );
+}
+export default ContactForm;
