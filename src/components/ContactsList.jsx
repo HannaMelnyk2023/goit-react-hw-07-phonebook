733981026f82f9d6eaba1c7b92ee1bb33a54a97a
HannaMelnyk2023 <151830548+HannaMelnyk2023@users.noreply.github.com>
2026-02-18 23:36:04 +0200

    edit ContactList

--- src/components/ContactsList.jsx
+++ src/components/ContactsList.jsx
@@ -1,17 +1,31 @@
-import {ContactsListItem} from 'components/ContactsListItem/ContactsListItem';
-export const ContactsList = ({ contacts, onDeleteContact, deleting }) => {
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteContact } from '../redux/contacts/operations';
+
+function ContactsList() {
+  const dispatch = useDispatch();
+  const contacts = useSelector(state => state.contacts);
+  const fiter = useSelector(state => state.filter);
+  const isLoading = useSelector(state => state.isLoading);
+  const error = useSelector(state => state.error);
+
+  const visibleContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(fiter.toLowerCase())
+  );
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
   return (
     <ul>
-      {contacts.map(contact => (
-        <ContactsListItem
-          key={contact.id}
-          id={contact.id}
-          name={contact.name}
-          number={contact.number}
-          onDeleteContact={onDeleteContact}
-          deleting={deleting}
-        />
+      {visibleContacts.map(contact => (
+        <li key={contact.id}>
+          {contact.name}: {contact.number}
+          {''}
+          <button onClick={() => dispatch(deleteContact(contact.id))}>
+            Delete
+          </button>
+        </li>
       ))}
     </ul>
   );
-};
+}
+export default ContactsList;
---
+++ src/components/Filter.jsx
@@ -0,0 +1,21 @@
+import { useDispatch, useSelector } from "react-redux";
+import { setFilter } from "../redux/filterSlice";
+
+function Filter() {
+    const dispatch = useDispatch();
+    const filter = useSelector(state => state.filter);
+
+    return (
+        <div>
+            <label>
+                Find number by name:
+                <input
+                    type="text"
+                    value={filter}
+                    onChange={e => dispatch(setFilter(e.target.value))}
+                />
+            </label>    
+        </div>  
+    );
+}
+export default Filter;
